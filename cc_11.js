f8aa1362b2c44a09a6d0041d4750b859b9764dbd
Karim <konanfoi@gmail.com>
2025-03-02 16:33:20 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -18,3 +18,27 @@ console.log(book1.getDetails());
 book1.updateCopies(-1);
 console.log(book1.getDetails());
 
+//Task 2 - Created Borrower Class
+class Borrower { //create a class with properties
+    constructor(name, borrowerId) {
+        this.name = name; //string
+        this.borrowerId = borrowerId; //number
+        this.borrowedBooks = []; //array of borrowed book titled
+    }
+    borrowBook(book) { //add a method
+        this.borrowedBooks.push(book);
+    }
+    returnBook(book) { //add a method
+        const index = this.borrowedBooks.indexOf(book);
+        if (index !== -1) {
+            this.borrowedBooks.splice(index, 1);
+        } else {
+            console.log(`${book} not in list`);
+        }
+    }
+}
+const borrower1 = new Borrower("Alice Johnson", 201); //test cases
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
